8386f2200bc217988519f46adcea60cbabf2073a
tim <timsregisters@gmail.com>
2022-05-11 12:28:23 +0200

    add two new tokens

--- main.py
+++ main.py
@@ -2,3 +2,5 @@ import os
 
 CONSUMER_KEY = os.getenv('twitter_consumer_key')
 SECRET_KEY = os.getenv('twitter_consumer_secret')
+ACCESS_TOKEN = os.getenv('twitter_access_token')
+ACCESS_TOKEN_SECRET = os.getenv('twitter_access_token_secret')
