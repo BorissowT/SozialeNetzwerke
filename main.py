36627f6cc628e0c658cf172de037690e7ee12380
tim <timsregisters@gmail.com>
2022-05-11 13:05:27 +0200

    first api request

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
 import os
+import tweepy
 
 CONSUMER_KEY = os.getenv('twitter_consumer_key')
 SECRET_KEY = os.getenv('twitter_consumer_secret')
 ACCESS_TOKEN = os.getenv('twitter_access_token')
 ACCESS_TOKEN_SECRET = os.getenv('twitter_access_token_secret')
+
+
+auth = tweepy.OAuthHandler(CONSUMER_KEY, SECRET_KEY)
+auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+api = tweepy.API(auth)
+
+cursor = tweepy.Cursor(api.search_tweets, q="CDU", tweet_mode="extended").items(1)
+
+for i in cursor:
+    print(i.full_text)
