8d18ba82bc9aab4815c03612a48698867a734b20
tim <timsregisters@gmail.com>
2022-05-11 22:04:35 +0200

    sunday scrape

---
+++ sunday_scrape.py
@@ -0,0 +1,21 @@
+from lxml import html
+import requests
+
+page = requests.get('https://www.wahlrecht.de/umfragen/dimap.htm')
+tree = html.fromstring(page.content)
+
+scrape = tree.xpath('//td/text()')
+list_with_integers = []
+for i in range(len(scrape)):
+    if scrape[i][0].isdigit():
+        list_with_integers.append(scrape[i])
+    print(scrape[i])
+    #if scrape[i][0] == ""
+chunk_size = 10
+chunked_list = []
+for i in range(0, len(list_with_integers), chunk_size):
+    chunked_list.append(list_with_integers[i:i+chunk_size])
+
+#for elem in chunked_list:
+#    print(elem[0])
+#print(list_with_integers)
